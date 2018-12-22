9af55119ee2ac4b7fe1dc08fbaf1713bce00878b
whitestne0746 <shiraishi.re1554@gmail.com>
2018-12-22 22:56:33 +0900

    f

--- src/index.js
+++ src/index.js
@@ -12,6 +12,7 @@ function init() {
   renderer.setPixelRatio(window.devicePixelRatio)
   renderer.setSize(windowWidth, windowHeight)
   renderer.setClearColor(0x40e0d0, 1.0)
+  renderer.shadowMap.enabled = true
   document.body.appendChild(renderer.domElement)
 
   const camera = new THREE.PerspectiveCamera(
@@ -27,24 +28,29 @@ function init() {
   const scene = new THREE.Scene()
 
   const light = new THREE.DirectionalLight('#ffffff')
+  light.castShadow = true
+  light.shadow.mapSize.width = 2048
+  light.shadow.mapSize.height = 2048
   light.intensity = 2
-  light.position.set(1, 1, 1)
+  light.position.set(0, 200, -200)
   scene.add(light)
 
-  /*
-  const amb = new THREE.AmbientLight('#464646')
-  scene.add(amb)
-  */
+  // const amb = new THREE.AmbientLight('#ffffff')
+  // scene.add(amb)
 
   const plane = new THREE.Mesh(
     new THREE.PlaneGeometry(100000, 100000),
     new THREE.MeshPhongMaterial({ color: 0xe9967a })
   )
+  plane.receiveShadow = true
   plane.rotation.x = -Math.PI / 2
   scene.add(plane)
 
   const building = new Building()
+  building.castShadow = true
+  building.receiveShadow = true
   building.position.x = -175
+  building.position.y = 5
   scene.add(building)
 
   animation()
